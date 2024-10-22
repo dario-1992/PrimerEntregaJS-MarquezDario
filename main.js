42e27b395d4f1aeb5b77135d03215b8cd25008f9
dario <dario.1992.marquez@gmail.com>
2024-10-21 23:46:33 -0300

    entrega n°1

---
+++ main.js
@@ -0,0 +1,98 @@
+
+class Producto {
+    constructor(nombre,valor,tipo) {
+        this.nombre = nombre
+        this.valor = valor
+        this.tipo = tipo
+    }
+}
+
+const mates= []; 
+
+const marshall = new Producto ("Marshall", 15000, "Amplificador")
+const peavey = new Producto("Peavey", 12000, "Amplificador")
+const DD7 = new Producto ("DD7", 12000, "Pedal")
+
+mates.push(marshall, peavey, DD7);
+
+
+const tazas= []; 
+
+const distortion = new Producto ("Distortion", 14000, "Pedal")
+const chorus = new Producto ("Chorus", 15500, "Pedal")
+const DD7t = new Producto ("DD7", 13000, "Pedal")
+
+tazas.push(distortion, chorus, DD7t);
+
+
+
+const porta= []; 
+
+const ts9 = new Producto ("TS9", 14000, "Pedal")
+const metalzone = new Producto ("MetalZone", 15500, "Pedal")
+const marshallp = new Producto ("Marshall", 13000, "Amplificador")
+
+porta.push(ts9, metalzone, marshallp);
+
+
+
+
+function solicitar() {
+    let nombre = prompt("Cual es tu nombre?");
+    alert("Bienvenido a MateZone: " + nombre);
+}
+solicitar()
+
+let opcion = prompt("Que producto estas buscando?:   \n 1= Mate \n 2= Taza \n 3 = Portapuas")
+if (opcion == 1) {
+let modelo = prompt("Que Mate buscas?:\n 1 = " + mates[0].nombre +  "\n 2 = " + mates[1].nombre + "\n 3 = " + mates[2].nombre)
+if (modelo == 1) {
+    confirm("Elegiste " + mates[0].nombre + "\n$ " + mates[0].valor + "\nTipo:" + mates[0].tipo);
+}
+else if (modelo == 2) {
+    confirm("Elegiste " + mates[1].nombre + "\n$ " + mates[1].valor + "\nTipo:" + mates[1].tipo);
+}
+
+else if (modelo == 3) {
+    confirm("Elegiste " + mates[2].nombre + "\n$ " + mates[2].valor + "\nTipo:" + mates[2].tipo);
+}
+}
+else if (opcion == 2) {
+    let modelo = prompt("Que Taza buscas?:\n 1 = " + tazas[0].nombre +  "\n 2 = " + tazas[1].nombre + "\n 3 = " + tazas[2].nombre)
+if (modelo == 1) {
+    confirm("Elegiste " + tazas[0].nombre + "\n$ " + tazas[0].valor + "\nTipo:" + tazas[0].tipo);
+}
+else if (modelo == 2) {
+    confirm("Elegiste " + tazas[1].nombre + "\n$ " + tazas[1].valor + "\nTipo:" + tazas[1].tipo);
+}
+
+else if (modelo == 3) {
+    confirm("Elegiste " + tazas[2].nombre + "\n$ " + tazas[2].valor + "\nTipo:" + tazas[2].tipo);
+}
+} else if (opcion == 3) {
+    let modelo = prompt("Que Portapuas buscas?:\n 1 = " + porta[0].nombre +  "\n 2 = " + porta[1].nombre + "\n 3 = " + porta[2].nombre)
+    if (modelo == 1) {
+        confirm("Elegiste " + porta[0].nombre + "\n$ " + porta[0].valor + "\nTipo:" + porta[0].tipo);
+    }
+    else if (modelo == 2) {
+        confirm("Elegiste " + porta[1].nombre + "\n$ " + porta[1].valor + "\nTipo:" + porta[1].tipo);
+    }
+    
+    else if (modelo == 3) {
+        confirm("Elegiste " + porta[2].nombre + "\n$ " + porta[2].valor + "\nTipo:" + porta[2].tipo);
+    };
+} else {
+    alert("Opción no válida.");
+}
+
+
+
+
+console.log(mates)
+console.log(tazas)
+console.log(porta)
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,43 @@
+# README - Página Web MateZone
+
+## Descripción General
+
+Bienvenido a MateZone, un sitio web dedicado a impresion de mates y portapuas en 3D.
+
+## Instrucciones de Uso
+
+### Inicio Rápido
+
+Visita nuestra página principal en https://matezone.netlify.app/  
+
+### Secciones Principales
+
+- **Mate Amp**: Mates impresos en 3d con forma de amplificadores de guitarra y bajo.![Mate Amp](./assets/mateamppeavey.jpg)
+- **Mate Pedal**: Mates impresos en 3d con forma de pedales.![Mate Pedal](./assets/materat.jpg)
+- **PortaPuas**: Portapuas o contenedores para puas, impresos en 3D, creados para disimularse en una pedalera.![Portapuas](./assets/portapuas-ts9.jpg)
+- **Preguntas**: Aqui podras ver las preguntas frecuentes y hacernos llegar tus dudas.
+- **Como lo hacemos?**: Mostramos nuestros primeros modelos desde el inicio, hasta los ultimos modelos que estan en desarrollo.
+
+### Tecnicas utilizadas
+
+-Flexbox
+-Grid
+-BS
+
+### Tecnologias
+
+-Html5
+-Css
+-Sass
+-Git
+-Nodejs
+
+
+## Contacto
+
+Para colaboraciones, preguntas o sugerencias, contáctanos a través de (dario.1992.marquez@gmail.com)
+
+## Créditos
+
+Desarrollado por Marquez Dario.
+
